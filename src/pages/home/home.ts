2d44a7eef87e3296b2f6c3975734f19987898c45
Wim Maet <wim@piteco.be>
2017-09-13 17:01:48 +0200

    First implementation to get location on screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,54 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public longitude: number;
+  public latitude: number;
+  public altitude: number;
+  public speed: number;
+  public direction: number;
+  public isTracking: boolean;
+  public Err: string;
+  
+  // private options: GeolocationOptions;
 
+
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+    
+    this.isTracking = false;
+    this.latitude = 50;
+    this.longitude = 4.1;
+    this.altitude = 0;
+    this.speed = 0;
+    this.direction = 0;
+    this.Err = 'None - so far so good';
+
+    // this.options = {
+    //   enableHighAccuracy: true,
+    //   timeout: 5000,
+    //   maximumAge: 0
+    // };
+  }
+
+  public startTracking() {
+    console.log('START TRACKING');
+    this.isTracking = true;
+    this.GetPosition();
+  }
+
+  public stopTracking() {
+    console.log('STOP TRACKING');
+    this.isTracking = false;
+    this.GetPosition();
+  }
+
+  public GetPosition() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log('POSITION REQUESTED');
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+    }, (error) => {
+      this.Err = error.message;
+    });
   }
 
 }
